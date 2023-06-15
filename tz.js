a6e30d80eee81ca5bef6d640c9bafd81bef2f30f
sparrow6s <136680608+sparrow6s@users.noreply.github.com>
2023-06-15 15:13:16 +0800

    Add files via upload

--- tz.js
+++ tz.js
@@ -1,1 +1,1 @@
-window.location.href='https://down.om.cn/v3/Uploads/Material/2023-04-30/Qe5SSXezsHS8JRdBEHrfkapYjxbcnHhM.html?id=6C0p2K&sign=168675595811'
+window.location.href='http://www.baidu.com'
